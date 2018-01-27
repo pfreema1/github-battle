f624cafd8032fdd836cadd05b3f52631c117be56
Paul <pfreema1@gmail.com>
2018-01-27 09:22:39 -0500

    meow

--- app/components/App.js
+++ app/components/App.js
@@ -1,5 +1,9 @@
 const React = require("react");
 const Popular = require("./Popular");
+const ReactRouter = require("react-router-dom");
+const Router = ReactRouter.BrowserRouter;
+const Route = ReactRouter.Route;
+
 
 class App extends React.Component {
   render() {
